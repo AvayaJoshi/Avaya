b594ce0fcf1f991f8237309713735f9070c259a1
AvayaJoshi <109412327+AvayaJoshi@users.noreply.github.com>
2022-07-16 19:04:13 +1000

    Add files via upload

---
+++ Character_count.py
@@ -0,0 +1,25 @@
+# Return the maximum occurring character
+ASCII_SIZE = 256
+ 
+def getMaxOccurringChar(str):
+
+    count = [0] * ASCII_SIZE
+ 
+
+    max = -1
+    c = ''
+ 
+
+    for i in str:
+        count[ord(i)]+=1;
+ 
+    for i in str:
+        if max < count[ord(i)]:
+            max = count[ord(i)]
+            c = i
+ 
+    return c
+ 
+str = input("Enter a string:")
+print("Max occurring character is:",getMaxOccurringChar(str))
+
